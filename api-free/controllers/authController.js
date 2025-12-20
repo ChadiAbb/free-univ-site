c60d8572214825c3d29f32974c499c3f64260d6a
Chadi ABBAD <chadi.abbad@gmail.com>
2025-12-20 23:21:27 +0100

    ajout du login

--- api-free/controllers/authController.js
+++ api-free/controllers/authController.js
@@ -1,21 +1,21 @@
-const connectDB = require("../config/db");
 const User = require("../models/User");
 const bcryptjs = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
 
 const register = async (req, res) => {
-    const {givenPseudo, givenPassword} = req.body;
+    const {pseudo, password} = req.body;
 
     try {
-        const userExists = await User.findOne({pseudo: givenPseudo})
+        const userExists = await User.findOne({pseudo: pseudo})
         if (userExists) {
             return res.status(400).json({ message: "Ce pseudo existe déjà" });
         }
 
         const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(givenPassword, salt);
+        const hashedPassword = await bcryptjs.hash(password, salt);
         
         const newUser = new User({
-            pseudo: givenPseudo, 
+            pseudo: pseudo, 
             password: hashedPassword
         });
 
@@ -27,4 +27,30 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = {register};
+const login = async (req, res) => {
+    const {pseudo, password} = req.body;
+
+    try {
+        const findUser = await User.findOne({pseudo: pseudo});
+        if (!findUser) {
+            return res.status(400).json({ message: "Identifiants invalides" });
+        }
+
+        if (!await bcryptjs.compare(password, findUser.password)) {
+            return res.status(400).json({message: "Identifiants invalides"});
+        }
+
+        const payload = { id: findUser._id };
+        const token = jsonwebtoken.sign(payload, process.env.JWT_SECRET, 
+            {expiresIn: '1d'});
+        
+        res.status(200).json({ 
+                message: "Connexion réussie", 
+                token: token 
+            });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur", error: error.message });
+    }
+}
+
+module.exports = {register, login};
--- api-free/server.js
+++ api-free/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 connectDB();
 
@@ -9,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Bienvenue sur l\'API!');
