6c447d305a3ce6163afa458142b52233ac9fb0ad
root <root@ip-10-8-72-178.eu-west-2.compute.internal>
2026-01-29 11:37:54 +0000

    changement de route dans api/free

--- api-free/routes/free.js
+++ api-free/routes/free.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const freeController = require("../controllers/freeController");
 
-router.get('/room', freeController.getFree);
+router.get('/', freeController.getFree);
 
 module.exports = router;
