2e603e2ece66186264a5826c771e30da3df19c35
root <root@ip-10-8-72-178.eu-west-2.compute.internal>
2026-02-04 13:59:16 +0000

    changement de href

--- front/login.js
+++ front/login.js
@@ -4,27 +4,27 @@ const errorMessage = document.getElementById("errorMessage");
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-        const pseudo = document.getElementById("pseudo").value;
-        const password = document.getElementById("password").value;
+    const pseudo = document.getElementById("pseudo").value;
+    const password = document.getElementById("password").value;
 
     try {
         const reponse = await fetch("http://80.247.3.232:8443/api/auth/login", 
             {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
                 body : JSON.stringify({
                     pseudo : pseudo,
-                    password : password
+                    password : password,
                 })
             }
-        )
+        );
         const data = await reponse.json();
 
         if (reponse.ok) {
             localStorage.setItem("token", data.token);
-            window.location.href("index.html");
+            window.location.href = "index.html";
         } else {
             errorMessage.innerText = "Mot de passe ou pseudo incorrect";
         }
